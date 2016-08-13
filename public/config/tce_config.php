0c220baad9768d9d2255d3f37276f073b5ec1c6e
satheesh <dannuri.satheesh@gmail.com>
2016-08-13 12:12:18 +0530

    configs are added

---
+++ public/config/tce_config.php
@@ -0,0 +1,137 @@
+<?php
+//============================================================+
+// File name   : tce_config.php
+// Begin       : 2001-10-23
+// Last Update : 2010-09-26
+//
+// Description : Configuration file for public section.
+//
+//
+// Author: Nicola Asuni
+//
+// (c) Copyright:
+//               Nicola Asuni
+//               Tecnick.com LTD
+//               www.tecnick.com
+//               info@tecnick.com
+//
+// License:
+//    Copyright (C) 2004-2010  Nicola Asuni - Tecnick.com LTD
+//    See LICENSE.TXT file for more information.
+//============================================================+
+
+/**
+ * @file
+ * Configuration file for public section.
+ * @package com.tecnick.tcexam.public.cfg
+ * @brief TCExam Configuration for Public Area
+ * @author Nicola Asuni
+ * @since 2001-10-23
+ */
+
+/**
+ */
+
+// --- INCLUDE FILES -----------------------------------------------------------
+
+require_once('../config/tce_auth.php');
+require_once('../../shared/config/tce_config.php');
+
+// --- DEFAULT META TAGS -------------------------------------------------------
+date_default_timezone_set('Asia/Kolkata');
+/**
+ * Default site name.
+ */
+define ('K_SITE_TITLE', 'TCExam');
+
+/**
+ * Default site description.
+ */
+define ('K_SITE_DESCRIPTION', 'TCExam by Tecnick.com');
+
+/**
+ * Default site author.
+ */
+define ('K_SITE_AUTHOR', 'Nicola Asuni - Tecnick.com LTD');
+
+/**
+ * Default html reply-to meta tag.
+ */
+define ('K_SITE_REPLY', ''); //
+
+/**
+ * Default keywords.
+ */
+define ('K_SITE_KEYWORDS', 'TCExam, eExam, e-exam, web, exam');
+
+/**
+ * Path to default html icon.
+ */
+define ('K_SITE_ICON', '../../favicon.ico');
+
+/**
+ * Path to public CSS stylesheet.
+ */
+define ('K_SITE_STYLE', K_PATH_STYLE_SHEETS.'default.css');
+define('K_JQUERY_CSS',K_PATH_STYLE_SHEETS.'jquery-ui.css');
+define('K_CALC_CSS',K_PATH_STYLE_SHEETS.'calclayout.css');
+/**
+ * Paths to js
+ *  */
+define('K_JQUERY',K_PATH_JSCRIPTS.'jquery-1.12.4.js');
+define('K_JQUERY_UI',K_PATH_JSCRIPTS.'jquery-ui.js');
+define('K_DIALOG_EXTEND',K_PATH_JSCRIPTS.'jquery.dialogextend.min.js');
+define('K_CUSTOM',K_PATH_JSCRIPTS.'custom.js');
+define('K_CALC',K_PATH_JSCRIPTS.'oscZenoedited.js');
+
+/**
+ * Full path to CSS stylesheet for RTL languages.
+ */
+define ('K_SITE_STYLE_RTL', K_PATH_STYLE_SHEETS.'default_rtl.css');
+
+// --- OPTIONS / COSTANTS ------------------------------------------------------
+
+/**
+ * Max number of rows to display in tables.
+ */
+define ('K_MAX_ROWS_PER_PAGE', 50);
+
+/**
+ * Max file size to be uploaded [bytes].
+ */
+define ('K_MAX_UPLOAD_SIZE', 1000000);
+
+/**
+ * Max memory limit for a PHP script.
+ */
+define ('K_MAX_MEMORY_LIMIT', '32M');
+
+/**
+ * Main page (homepage).
+ */
+define ('K_MAIN_PAGE', 'index.php');
+
+/**
+ * Enable PDF results on public area.
+ */
+define ('K_ENABLE_PUBLIC_PDF', true);
+
+/**
+ * If true hide the expired tests from index table.
+ */
+define ('K_HIDE_EXPIRED_TESTS', false);
+
+// --- INCLUDE FILES -----------------------------------------------------------
+
+require_once('../../shared/config/tce_db_config.php');
+require_once('../../shared/code/tce_db_connect.php');
+require_once('../../shared/code/tce_functions_general.php');
+
+// --- PHP SETTINGS ------------------------------------------------------------
+
+ini_set('memory_limit', K_MAX_MEMORY_LIMIT); // set PHP memory limit
+ini_set('session.use_trans_sid', 0); // if =1 use PHPSESSID (for clients that do not support cookies)
+
+//============================================================+
+// END OF FILE
+//============================================================+
---
+++ shared/config/tce_user_registration.php
@@ -0,0 +1,78 @@
+<?php
+//============================================================+
+// File name   : tce_user_registration.php
+// Begin       : 2008-03-30
+// Last Update : 2009-11-10
+//
+// Description : Configuration file for user registration.
+//
+// Author: Nicola Asuni
+//
+// (c) Copyright:
+//               Nicola Asuni
+//               Tecnick.com LTD
+//               www.tecnick.com
+//               info@tecnick.com
+//
+// License:
+//    Copyright (C) 2004-2010  Nicola Asuni - Tecnick.com LTD
+//    See LICENSE.TXT file for more information.
+//============================================================+
+
+/**
+ * @file
+ * Configuration file for user registration.
+ * NOTE: the email verification template is stored on the
+ * TMX file at "m_email_registration" translation unit.
+ * @package com.tecnick.tcexam.shared.cfg
+ * @author Nicola Asuni
+ * @since 2008-03-30
+ */
+
+/**
+ * If true enable USER REGISTRATION.
+ */
+define ('K_USRREG_ENABLED', true);
+
+/**
+ * If true requires email confirmation.
+ */
+define ('K_USRREG_EMAIL_CONFIRM', true);
+
+/**
+ * Default user group ID for registered user.
+ */
+define ('K_USRREG_GROUP', 1);
+
+/**
+ * URL of an HTML page containing the registration agreement (i.e.: "http://www.yoursite.com/agreement.html").
+ */
+define ('K_USRREG_AGREEMENT', '');
+
+/**
+ * The following email will receive copies of verification messages.
+ */
+define ('K_USRREG_ADMIN_EMAIL', '');
+
+/**
+ * Additional fields to display on registration form.
+ * Legal values are:
+ * 0 = disabled field;
+ * 1 = enabled field;
+ * 2 = required field;
+ */
+$regfields = array(
+	'user_email' => 2,
+	'user_regnumber' => 1,
+	'user_firstname' => 2,
+	'user_lastname' => 2,
+	'user_birthdate' => 1,
+	'user_birthplace' => 1,
+	'user_ssn' => 1,
+	'user_groups' => 1,
+	'user_agreement' => 2
+);
+
+//============================================================+
+// END OF FILE
+//============================================================+
